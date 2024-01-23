4cd3dedd8f3d407339055d0649998195f7473e53
itsabhinavism <a.for.abhinav.2003@gmail.com>
2024-01-23 17:18:13 +0530

    save

---
+++ 1/q1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class q1 {
+
+  public static void main(String[] args) {
+    String name = "Abhinav Anand";
+    String rollNo = "22052611";
+    String section = "CSE-09";
+    String branch = "CSE";
+
+    System.out.println("Name: " + name);
+    System.out.println("Roll Number: " + rollNo);
+    System.out.println("Section: " + section);
+    System.out.println("Branch: " + branch);
+  }
+}
---
+++ 1/q2.java
@@ -0,0 +1,19 @@
+public class q2 {
+    public static void main(String[] args) {
+        int mark = 85;  // Replace with the actual mark
+
+        if (mark >= 90) {
+            System.out.println("Grade: A+");
+        } else if (mark >= 80) {
+            System.out.println("Grade: A");
+        } else if (mark >= 70) {
+            System.out.println("Grade: B");
+        } else if (mark >= 60) {
+            System.out.println("Grade: C");
+        } else if (mark >= 50) {
+            System.out.println("Grade: D");
+        } else {
+            System.out.println("Grade: F");
+        }
+    }
+}
